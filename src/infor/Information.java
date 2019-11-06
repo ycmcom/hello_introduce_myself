9d415d74d535ff6796505da4828093fae7b8d23a
ycm <ycmcom@naver.com>
2019-11-06 09:40:31 +0900

    infor

---
+++ src/infor/Information.java
@@ -0,0 +1,10 @@
+package infor;
+
+public class Information {
+
+	public static void main(String[] args) {
+		System.out.println("ComputerSW");
+		System.out.println("Wonkwang Uni");
+		System.out.println("¾çÃ¶¹Î");
+	}
+}
